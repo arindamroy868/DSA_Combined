1a81cdebb2df29d9aa8c766550ec60a19b76c047
Aman2198 <amanbahal.21@gmail.com>
2021-09-10 23:56:51 +0530

    Solved 1 program

---
+++ Arrays/Union_Of_Array/UnionOf2Array.java
@@ -0,0 +1,36 @@
+package com.dsa.arrays;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class UnionOf2Array {
+    static void unionCount(int a[], int b[],int n, int m) {
+        Map<Integer,Integer> mp = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < n; i++) {
+            mp.put(a[i], i);
+        }
+        for(int i = 0; i < m; i++) {
+            mp.put(b[i], i);
+        }
+        System.out.println(mp.size());
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        int[] b = new int[10];
+        int n,m,i=0;
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter Number of inputs");
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(i=0;i<n;i++)
+            a[i] = sc.nextInt();
+        for(i=0;i<m;i++)
+            b[i] = sc.nextInt();
+        unionCount(a,b,n,m);
+    }
+}
