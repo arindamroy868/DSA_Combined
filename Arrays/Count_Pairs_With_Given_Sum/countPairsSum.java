59c8cce418377dee0ae3e26f4f12d3f30f527139
Aman2198 <amanbahal.21@gmail.com>
2021-09-17 00:00:27 +0530

    Done 1 problem

---
+++ Arrays/Count_Pairs_With_Given_Sum/countPairsSum.java
@@ -0,0 +1,40 @@
+package com.dsa.arrays;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class countPairsSum {
+    public static void getPairsCount(int arr[], int n, int k){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0; i<n;i++){
+            if(!hm.containsKey(arr[i])){
+                hm.put(arr[i],0);
+            }
+            hm.put(arr[i],hm.get(arr[i]+1));
+        }
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(hm.get(k-arr[i])!=null){
+                count = count+hm.get(k-arr[i]);
+                if (k - arr[i] == arr[i]) {
+                    count--;
+                }
+            }
+
+        }
+        System.out.println(count/2);
+    }
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        int n,i=0,k;
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter Number of inputs");
+        n = sc.nextInt();
+        System.out.println("Sum");
+        k = sc.nextInt();
+        for(i=0;i<n;i++)
+            a[i] = sc.nextInt();
+        getPairsCount(a,n,k);
+    }
+}
+
