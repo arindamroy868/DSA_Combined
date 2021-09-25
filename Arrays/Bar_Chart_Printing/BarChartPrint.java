2d97044227566160da6aa3524a4e923d2e2ef81c
Arindam Ray <arindamroy.868@gmail.com>
2021-09-25 12:19:59 +0530

    Added Java Solution

---
+++ Arrays/Bar_Chart_Printing/BarChartPrint.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.lang.*;
+
+public class BarChartPrint {
+     public static void main(String[] args) throws Exception {
+            // write your code here
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            int max = -1;
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+                max = Math.max(max,arr[i]);
+            }
+
+            while(max>0){
+                for(int j=0;j<n;j++){
+                    if(arr[j]>=max){
+                        System.out.print("*\t");
+                    }else{
+                        System.out.print("\t");
+                    }
+                }
+                max--;
+                System.out.println();
+            }
+     }
+}
