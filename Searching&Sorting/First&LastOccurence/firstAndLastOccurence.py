e61a685e0bdc88e116450257a34fd27dc4d8cd2f
inquiridortechie <narya436.na@gmail.com>
2021-10-01 21:07:20 +0530

    Updated python solution

--- Searching&Sorting/First&LastOccurence/firstAndLastOccurence.py
+++ Searching&Sorting/First&LastOccurence/firstAndLastOccurence.py
@@ -1,15 +1,61 @@
 # Method 1 (Naive Approach) Time complexity - O(n) and Space Complexity - O(1)
 def firstAndLastOccurence1(arr, k):
-    result = [-1, len(arr)-1]
+    result = [-1, -1]
     for i in range(len(arr)):
         if arr[i] == k and result[0] == -1:
             result[0] = i
-        elif result[0] != -1 and arr[i] != k:
-            result[1] = i-1
-            break
+            result[1] = i
+        elif arr[i] == k:
+            result[1] = i
 
     if result[0] != -1:
-        print("First and Last occurence of ",k," : ", end = ' ')
+        print("First and Last index of ",k," using naive approach : ", end =' ')
+        for i in result:
+            print(i, end= ' ')
+        print()
+    else:
+        print("Element not in the list")
+            
+
+# Method 2 (Efficient Approach) Time Complexity - O(logn) and Space Complexity - O(1)
+def firstAndLastOccurence2(arr, k):
+    l = 0
+    h = len(arr) - 1
+    first = -1
+    last = -1
+    result = []
+
+    # First Occurence Logic
+    while (l <= h):
+        mid = (l + h) // 2
+
+        if arr[mid] > k:
+            h = mid - 1
+        elif arr[mid] < k:
+            l = mid + 1
+        else:
+            first = mid
+            h = mid - 1
+    
+    result.append(first)
+    l = 0
+    h = len(arr) - 1
+
+    # Last Occurence Logic
+    while(l <= h):
+        mid = (l + h) // 2
+         
+        if arr[mid] > k:
+            h = mid - 1
+        elif arr[mid] < k:
+            l = mid + 1
+        else:
+            last = mid
+            l = mid + 1
+
+    result.append(last)
+    if result[0] != -1:
+        print("First and Last index of ",k," using efficient approach : ", end=' ')
         for i in result:
             print(i, end= ' ')
         print()
@@ -17,6 +63,7 @@ def firstAndLastOccurence1(arr, k):
         print("Element not in the list")
 
 
-arr = [1, 3, 5, 5, 5, 5, 7, 123, 125]
-k = 7
+arr = list(map(int, input("Enter the elements in the list : ").split()))
+k = int(input("Enter the element whose occurence need to find : "))
 firstAndLastOccurence1(arr, k)
+firstAndLastOccurence2(arr, k)
