c7e27c7f869fb39dc8fe44791ec4a3dc9320d032
inquiridortechie <narya436.na@gmail.com>
2021-10-09 23:47:56 +0530

    feat(longestPalindromeSubstring):Updated python solution.
    1. Added Dynamic Programming and Expand around corner approach.

--- String/Longest_Palindromic_Substring/longestPalindromeSubstring.py
+++ String/Longest_Palindromic_Substring/longestPalindromeSubstring.py
@@ -1,5 +1,7 @@
 # Method 1 (Naive Apporach) - Time Complexity - O(N^3) and Space Complexity - O(1)
-def longestPlaindrome(s):
+def longestPlaindrome1(s):
+    if s is None or len(s) < 1:
+        return ''
     n = len(s)
     maxLength = 0
     maxPlaindrome = s[0]
@@ -16,5 +18,69 @@ def longestPlaindrome(s):
     
     return maxPlaindrome
 
+
+# Method 2 (Dynamic Programming) - Time Complexity - O(N^2) and Space Complexity - O(N^2)
+def longestPalindrome2(s):
+    if s is None or len(s) < 1:
+        return ''
+    n = len(s)
+    maxLength = 0
+    maxPlaindrome = s[0]
+    dp = [[1 if i == j else 0 for i in range(n)] for j in range(n)]
+
+    # check string of length 2
+    for i in range(n-1):
+        if s[i] == s[i+1]:
+            dp[i][i+1] = 1
+            if 2 > maxLength:
+                maxLength = 2
+                maxPlaindrome = s[i:i+2]
+        else:
+            dp[i][i+1] = 0
+
+    j = 2
+    while j < n:
+        i = 0
+        while i < j-1:
+            if dp[i+1][j-1] == 1 and s[i] == s[j]:
+                dp[i][j] = 1
+
+                if j-i+1 > maxLength:
+                    maxLength = j-i+1
+                    maxPlaindrome = s[i:j+1]   
+            i += 1
+        j += 1
+
+    return maxPlaindrome
+
+
+# Method 3 (Expand Around Center) - Time Complexity - O(N^2) and Space Complexity - O(1)
+def longestPalindrome3(s):
+    if s is None or len(s) < 1:
+        return ''
+    n = len(s)
+    maxLength = 1
+    maxPlaindrome = s[0]
+    for i in range(n):
+        len1 = expandAroundCenter(s, i, i)
+        len2 = expandAroundCenter(s, i, i+1)
+
+        if max(len1, len2) > maxLength:
+            maxLength = max(len1, len2)
+            start = i - (maxLength -1) // 2
+            end = i + maxLength // 2
+            maxPlaindrome = s[start : end+1]
+
+    return maxPlaindrome
+        
+
+def expandAroundCenter(s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+
+    return right - left - 1
+
+
 s = input("Enter the string : ")
-print("Longest palindrome in the string is : ",longestPlaindrome('aaaabbaa'))
+print("Longest palindrome in the string is : ",longestPalindrome3(s))
