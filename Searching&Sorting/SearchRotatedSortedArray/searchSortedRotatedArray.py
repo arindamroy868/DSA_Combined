a73f48e88b1f628933b4312a0f32ed203781fc3d
inquiridortechie <narya436.na@gmail.com>
2021-10-18 23:56:59 +0530

    Updated python solution
    1. Add efficient approach in search element in sorted and rotated array.

--- Searching&Sorting/SearchRotatedSortedArray/searchSortedRotatedArray.py
+++ Searching&Sorting/SearchRotatedSortedArray/searchSortedRotatedArray.py
@@ -1,6 +1,6 @@
 # Method 1 (Binary Search) - Time Complexity - O(log n) and Space Complexity - O(1)
 class Search:
-    def serachInRotatedArray(self, nums, n, target):
+    def searchInRotatedArray(self, nums, n, target):
         pivot = self.findPivot(nums, 0, n-1);
         low = 0
         high = n-1
@@ -43,8 +43,29 @@ class Search:
         return self.findPivot(arr, mid + 1, high)
 
 
+# Method 2 (Efficient approach) - Time Complexity - O(logn) and Space Complexity - O(1)
+# Reduced the no. of binary search
+def searchInRotatedArray(arr, l, h, target):
+    if l > h:
+        return -1
+    
+    mid = (l+h)//2
+    if arr[mid] == target:
+        return mid
+    
+    if arr[l] <= arr[mid]:
+        if target <= arr[mid] and target >= arr[l]:
+            return searchInRotatedArray(arr, l, mid-1, target)
+        return searchInRotatedArray(arr, mid+1, h, target)
+    
+    if target >= arr[mid] and target <= arr[h]:
+        return searchInRotatedArray(arr, mid+1, h, target)
+    return searchInRotatedArray(arr, l, mid-1, target)
+
+
 arr = list(map(int, input("Enter the elements in the list : ").split()))
 n = len(arr)
 target = int(input("Enter the target value : "))
 obj = Search()
-print("Index of the element is : ", obj.serachInRotatedArray(arr, n, target))
+print("Index of the element using Method 1 : ", obj.searchInRotatedArray(arr, n, target))
+print("Index of the element using Method 2 : ", searchInRotatedArray(arr, 0, n-1, target))
