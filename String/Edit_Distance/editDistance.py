723651dbfd3a900be8c54b2725498865f38f8142
inquiridortechie <narya436.na@gmail.com>
2021-10-20 23:57:23 +0530

    Added python solution
    1. Edit Distance problem.

---
+++ String/Edit_Distance/editDistance.py
@@ -0,0 +1,27 @@
+# Method 1 (Dynamic Programming) - Time Complexity - O(mn) and Space Complexity - O(mn)
+# m - length of string 1
+# n - length of string 2
+
+def editDistance(s1, s2):
+    m = len(s1)
+    n = len(s2)
+    dp = [[0 for j in range(n+1)] for i in range(m+1)]
+    for i in range(m+1):
+        dp[i][0] = i
+
+    for j in range(n+1):
+        dp[0][j] = j
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]))
+
+    return dp[m][n]
+
+
+s1 = input("Enter word1 : ")
+s2 = input("Enter word2 : ")
+print("Minimum operation to convert word1 to word 2 : ", editDistance(s1, s2))
