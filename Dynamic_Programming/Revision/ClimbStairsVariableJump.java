efee62f3981165d7b513d1a7f158ec88003d3c49
arindam <arindamroy.868@gmail.com>
2021-11-01 23:55:01 +0530

    Added Java Solution

---
+++ Dynamic_Programming/Revision/ClimbStairsVariableJump.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ClimbStairsVariableJump {
+    public static int climbStairsVariableJump(int n,int[] arr){
+        int[] dp = new int[n+1];
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
+            int paths = 0;
+            for(int j=0;j<=arr[i] && i+j<=n;j++){
+                paths+=dp[i+j];
+            }
+            dp[i] = paths;
+        }
+        return dp[0];
+    }
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++){
+                st = new StringTokenizer(br.readLine());
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            System.out.println(climbStairsVariableJump(n,arr));
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
