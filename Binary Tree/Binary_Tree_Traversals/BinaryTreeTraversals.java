5e84ba5631bb8e19a750cb50e4f7c979620c474a
Arindam Ray <arindamroy.868@gmail.com>
2021-12-30 02:40:05 +0530

    Added Postorder Iterative Solution

--- Binary Tree/Binary_Tree_Traversals/BinaryTreeTraversals.java
+++ Binary Tree/Binary_Tree_Traversals/BinaryTreeTraversals.java
@@ -1,5 +1,7 @@
 package Binary_Tree_Traversals;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 import Node.Node;
@@ -13,18 +15,22 @@ public class BinaryTreeTraversals {
         preorderTraversalRecursion(root.right);
     }
 
-    public static void preorderTraversalIterative(Node root){
+    public static List<Integer> preorderTraversal(Node root) {
+        List<Integer> pre = new ArrayList<>();
         Stack<Node> st = new Stack<>();
+        
         while(true){
             if(root != null){
-                System.out.print(root.data + " ");
-                if(root.right != null) st.push(root.right);
-                root = root.left;
+                pre.add(root.data);
+                if(root.right != null)st.push(root.right);
+                root =  root.left;
+                   
             }else{
-                if(st.isEmpty()) break;
-                root = st.pop();
+                    if(st.isEmpty()) break;
+                    root = st.pop();
+                }
             }
-        }
+        return pre;
     }
 
     public static void inorderTraversalRecursion(Node root){
@@ -34,8 +40,10 @@ public class BinaryTreeTraversals {
         inorderTraversalRecursion(root.right);
     }
 
-    public static void inorderTraversalIterative(Node root){
-        Stack<Node> st = new Stack<>();
+    public static List<Integer> inorderTraversal(Node root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<Node> st = new Stack<Node>();
+        
         while(true){
             if(root != null){
                 st.push(root);
@@ -43,10 +51,11 @@ public class BinaryTreeTraversals {
             }else{
                 if(st.isEmpty()) break;
                 root = st.pop();
-                System.out.print(root.data + " ");
+                list.add(root.data);
                 root = root.right;
             }
         }
+        return list;
     }
 
     public static void postorderTraversalRecursion(Node root){
@@ -56,6 +65,31 @@ public class BinaryTreeTraversals {
         System.out.print(root.data +" ");        
     }
 
+    public static List<Integer> postorderTraversal(Node root) {
+        Stack<Node> st = new Stack<Node>();
+        List<Integer> post =  new ArrayList<Integer>();
+        Node curr = root;
+        Node temp = null;
+        while(curr != null || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }else{
+                temp = st.peek().right;
+                if(temp == null){
+                    temp = st.pop();
+                    post.add(temp.data);
+                    while(!st.isEmpty() && st.peek().right == temp){
+                        temp = st.pop();
+                        post.add(temp.data);
+                    }
+                }else{
+                    curr = temp;
+                }
+            }
+        }
+        return post;
+    }
     public static void main(String[] args) {
         Node node = new Node(1);
         node.left = new Node(2);
@@ -67,12 +101,16 @@ public class BinaryTreeTraversals {
 
         preorderTraversalRecursion(node);
         System.out.println();
-        preorderTraversalIterative(node);
+        System.out.println(preorderTraversal(node));
         System.out.println();
         inorderTraversalRecursion(node);
         System.out.println();
-        inorderTraversalIterative(node);
+        System.out.println(inorderTraversal(node)); 
         System.out.println();
         postorderTraversalRecursion(node);
+        System.out.println();
+        System.out.println(postorderTraversal(node));
+        System.out.println();
+
     }
 }
