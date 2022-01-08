497393034b71927fa240e2a45394300d7ec47a46
Arindam Ray <arindamroy.868@gmail.com>
2022-01-08 15:44:11 +0530

    Vertical Order Traversal in O(N) Time

--- Binary Tree/Vertical_Order_Traversal/VerticalOrderTraversal.java
+++ Binary Tree/Vertical_Order_Traversal/VerticalOrderTraversal.java
@@ -2,6 +2,8 @@ package Vertical_Order_Traversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 //Definition for a binary tree node.
 class TreeNode {
@@ -18,7 +20,7 @@ class TreeNode {
 }
 
 public class VerticalOrderTraversal {
-    
+
     static Integer min = Integer.MAX_VALUE;
     static Integer max = Integer.MIN_VALUE;
 
@@ -32,6 +34,9 @@ public class VerticalOrderTraversal {
         findMinMaxLevels(root.right,hd+1);
     }
 
+    //Prints the vertical order of BT but does not maintain the vertical order of nodes in a line.
+    //Time = O(N*2)-> For each horizontal level it visits n nodes. So for n horizontal levels it will take O(N) * O(N).
+    //Space = O(N) -> Stack Trace
     public static void printVerticalOrderTraversal(TreeNode root,int hd,int level){
         if(root == null) return;
 
@@ -41,6 +46,56 @@ public class VerticalOrderTraversal {
         printVerticalOrderTraversal(root.left, hd, level-1);
         printVerticalOrderTraversal(root.right, hd, level+1);
     }
+
+    public static void verticalOrderTraversal(TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map,int hd,int vd,TreeNode node){
+        if(node == null) return;
+
+        if(!map.containsKey(hd)){
+            map.put(hd, new TreeMap<>());
+        }
+        if(!map.get(hd).containsKey(vd)){
+            map.get(hd).put(vd,new PriorityQueue<>());
+        }
+
+        TreeMap<Integer, PriorityQueue<Integer>> verticalMap = map.get(hd);
+        PriorityQueue<Integer> list = verticalMap.get(vd);
+        list.add(node.val);
+        verticalMap.put(vd,list);
+        map.put(hd,verticalMap);
+
+        verticalOrderTraversal(map, hd-1, vd+1, node.left);
+        verticalOrderTraversal(map, hd+1, vd+1, node.right);
+    }
+
+    //Prints the vertical order of BT and maintains the vertical order of Nodes in a vertical line.
+    //Time = O(N) -> Because travels each node only once
+    //Space = O(N) -> Stack Trace
+    public static void printVerticalOrderTraversal2(TreeNode root){
+        if(root == null) return;
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
+        verticalOrderTraversal(map, 0, 0, root);
+
+        List<List<Integer>> list = new ArrayList<>();
+        
+        for(TreeMap<Integer, PriorityQueue<Integer>> levelMap : map.values()){
+            List<Integer> levelList = new ArrayList<>();
+            for(PriorityQueue<Integer> q : levelMap.values()){
+                while(!q.isEmpty()){
+                    levelList.add(q.poll());
+                }
+            }
+            list.add(levelList);
+        }
+        
+        for(List<Integer> integerList : list ){
+            for(Integer val : integerList){
+                System.out.print(val +" ");
+            }
+            System.out.println();
+        }
+        
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
@@ -52,11 +107,7 @@ public class VerticalOrderTraversal {
         root.right.left =  new TreeNode(9);
         root.right.right =  new TreeNode(10);
 
-        findMinMaxLevels(root, 0);
-        for(int hd = min;hd<=max;hd++){
-            printVerticalOrderTraversal(root, hd , 0);
-            System.out.println();
-        }
+        printVerticalOrderTraversal2(root);
 
     }
 }
