3008eaab66f13e4ce418b6d875fbcdf3f269a707
Arindam Ray <arindamroy.868@gmail.com>
2022-01-28 23:38:12 +0530

    Find Ceil of Value in BST

---
+++ Binary Tree/Ceil_Floor_Of_BST/CeilAndFloorBST.java
@@ -0,0 +1,48 @@
+package Ceil_Floor_Of_BST;
+
+class TreeNode {
+    Integer val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(Integer val) { this.val = val; }
+    TreeNode(Integer val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class CeilAndFloorBST {
+    public static TreeNode ceilValueNode(int value,TreeNode root){
+        if(root == null) return null;
+        TreeNode ceil = null;
+        TreeNode current = root;
+        while(current != null){
+            if(current.val >= value){
+                if(ceil == null){
+                    ceil = current;
+                }else{
+                    ceil = current.val < ceil.val?current:ceil;
+                }
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return ceil;
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.right = new TreeNode(13);
+        root.right.left = new TreeNode(12);
+        root.right.right = new TreeNode(15);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(6);
+        root.left.right.right = new TreeNode(7);
+        TreeNode res = ceilValueNode(13,root);
+        System.out.println(res!=null?res.val:"null");
+    }
+
+}
